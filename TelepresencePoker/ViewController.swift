79a92d0fc5a266c15c508c9055e88d2d60c950e3
bschmuck <bschmuck@andrew.cmu.edu>
2018-01-16 20:25:02 -0500

    Initial Commit

---
+++ TelepresencePoker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TelepresencePoker
+//
+//  Created by Brandon Schmuck on 1/16/18.
+//  Copyright © 2018 Brandon Schmuck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
