b6f4072ba8a2e229a02efb49a68747eb2aa88ce0
Amolak Nagi <amolakn@andrew.cmu.edu>
2018-01-18 16:01:04 -0500

    Integrated video chat, it works!

--- TelepresencePoker/TPMainViewController.swift
+++ TelepresencePoker/TPMainViewController.swift
@@ -7,8 +7,39 @@
 //
 
 import UIKit
+import TwilioVideo
 
 class TPMainViewController: UIViewController {
+    
+    struct PlatformUtils {
+        static let isSimulator: Bool = {
+            var isSim = false
+            #if arch(i386) || arch(x86_64)
+                isSim = true
+            #endif
+            return isSim
+        }()
+    }
+    
+    // Client Identity is "player_iphone"
+    var player_iphone_accessToken: String = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2RlZTM4YmU3MGVhOTllMTA5MzE1MzRkODdkZTRlNmNlLTE1MTYzMDc4MzMiLCJpc3MiOiJTS2RlZTM4YmU3MGVhOTllMTA5MzE1MzRkODdkZTRlNmNlIiwic3ViIjoiQUNjMWYzNWQzNzQzYzA5MjIwMWM5ZTUyN2MyMTM3Y2QxNSIsImV4cCI6MTUxNjMxMTQzMywiZ3JhbnRzIjp7ImlkZW50aXR5IjoicGxheWVyX2lwaG9uZSIsInZpZGVvIjp7fX19.KcL8iYvGhF4AWrsCagByfAkjPmQRj3CnkbcweboEoTs"
+    
+    var board_ipad_accessToken: String = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2RlZTM4YmU3MGVhOTllMTA5MzE1MzRkODdkZTRlNmNlLTE1MTYzMDc3MzQiLCJpc3MiOiJTS2RlZTM4YmU3MGVhOTllMTA5MzE1MzRkODdkZTRlNmNlIiwic3ViIjoiQUNjMWYzNWQzNzQzYzA5MjIwMWM5ZTUyN2MyMTM3Y2QxNSIsImV4cCI6MTUxNjMxMTMzNCwiZ3JhbnRzIjp7ImlkZW50aXR5IjoiYm9hcmRfaXBhZCIsInZpZGVvIjp7fX19.4gLSRiKvn4neBwqHYquTEO_bjTcNU8ZfTfzW1blqR08"
+    
+    
+    // Video SDK components
+    var room: TVIRoom?
+    var camera: TVICameraCapturer?
+    var localVideoTrack: TVILocalVideoTrack?
+    var localAudioTrack: TVILocalAudioTrack?
+    var participant: TVIParticipant?
+    var remoteView: TVIVideoView?
+    
+    // UI Element Outlets and handles
+    @IBOutlet weak var previewView: TVIVideoView!
+    
+    
+
 
     @IBOutlet weak var betButton: UIButton!
     @IBOutlet weak var raiseButton: UIButton!
@@ -43,6 +74,12 @@ class TPMainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if PlatformUtils.isSimulator {
+            // Do nothing
+        } else {
+            self.startPreview()
+        }
         raiseButton.layer.cornerRadius = 15
         betButton.layer.cornerRadius = 15
         foldButton.layer.cornerRadius = 15
@@ -58,6 +95,145 @@ class TPMainViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(updateGameState), name: NSNotification.Name(rawValue: "GameStateDidUpdate"), object: nil)
         
         stateManager.getGameState()
+    }
+    
+    func startPreview() {
+        if PlatformUtils.isSimulator {
+            return
+        }
+        
+        // Preview our local camera track in the local video preview view.
+        camera = TVICameraCapturer(source: .frontCamera, delegate: self)
+        localVideoTrack = TVILocalVideoTrack.init(capturer: camera!)
+        if (localVideoTrack == nil) {
+            print("Failed to create video track")
+        } else {
+            // Add renderer to video track for local preview
+            localVideoTrack!.addRenderer(self.previewView)
+            
+            print("Video track created")
+            
+            // We will flip camera on tap.
+            //let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.flipCamera))
+            //self.previewView.addGestureRecognizer(tap)
+        }
+    }
+    
+    func setupRemoteVideoView() {
+        // Creating `TVIVideoView` programmatically
+        self.remoteView = TVIVideoView.init(frame: CGRect.zero, delegate:self)
+        
+        self.view.insertSubview(self.remoteView!, at: 0)
+        
+        // `TVIVideoView` supports scaleToFill, scaleAspectFill and scaleAspectFit
+        // scaleAspectFit is the default mode when you create `TVIVideoView` programmatically.
+        self.remoteView!.contentMode = .scaleAspectFit;
+        
+        let centerX = NSLayoutConstraint(item: self.remoteView!,
+                                         attribute: NSLayoutAttribute.centerX,
+                                         relatedBy: NSLayoutRelation.equal,
+                                         toItem: self.view,
+                                         attribute: NSLayoutAttribute.centerX,
+                                         multiplier: 1,
+                                         constant: 0);
+        self.view.addConstraint(centerX)
+        let centerY = NSLayoutConstraint(item: self.remoteView!,
+                                         attribute: NSLayoutAttribute.centerY,
+                                         relatedBy: NSLayoutRelation.equal,
+                                         toItem: self.view,
+                                         attribute: NSLayoutAttribute.centerY,
+                                         multiplier: 1,
+                                         constant: 0);
+        self.view.addConstraint(centerY)
+        let width = NSLayoutConstraint(item: self.remoteView!,
+                                       attribute: NSLayoutAttribute.width,
+                                       relatedBy: NSLayoutRelation.equal,
+                                       toItem: self.view,
+                                       attribute: NSLayoutAttribute.width,
+                                       multiplier: 1,
+                                       constant: 0);
+        self.view.addConstraint(width)
+        let height = NSLayoutConstraint(item: self.remoteView!,
+                                        attribute: NSLayoutAttribute.height,
+                                        relatedBy: NSLayoutRelation.equal,
+                                        toItem: self.view,
+                                        attribute: NSLayoutAttribute.height,
+                                        multiplier: 1,
+                                        constant: 0);
+        self.view.addConstraint(height)
+        
+    }
+
+    @IBAction func pressedStartChat(_ sender: Any) {
+        var accessToken: String = ""
+        
+        if PlatformUtils.isSimulator {
+            accessToken = board_ipad_accessToken
+        } else {
+            accessToken = player_iphone_accessToken
+        }
+        //        var accessToken: String = player_iphone_accessToken
+        
+        self.prepareLocalMedia()
+        
+        // Preparing the connect options with the access token that we fetched (or hardcoded).
+        let connectOptions = TVIConnectOptions.init(token: accessToken) { (builder) in
+            
+            // Use the local media that we prepared earlier.
+            builder.audioTracks = self.localAudioTrack != nil ? [self.localAudioTrack!] : [TVILocalAudioTrack]()
+            builder.videoTracks = self.localVideoTrack != nil ? [self.localVideoTrack!] : [TVILocalVideoTrack]()
+            
+            // The name of the Room where the Client will attempt to connect to. Please note that if you pass an empty
+            // Room `name`, the Client will create one for you. You can get the name or sid from any connected Room.
+            builder.roomName = "TEST_ROOM"//self.roomTextField.text
+        }
+        
+        // Connect to the Room using the options we provided.
+        room = TwilioVideo.connect(with: connectOptions, delegate: self)
+        
+        print("Attempting to connect to room \(String(describing: "TEST_ROOM"))")
+        
+        //        self.showRoomUI(inRoom: true)
+        //        self.dismissKeyboard()
+        
+        
+        //        var localVideoTrack : TVILocalVideoTrack?
+        //
+        //        if let camera = TVICameraCapturer(source: .frontCamera) {
+        //            localVideoTrack = TVILocalVideoTrack.init(capturer: camera)
+        //        } else {
+        //            print("Camera initialization failed")
+        //        }
+        //
+        //        let connectOptions = TVIConnectOptions.init(token: player_iphone_accessToken) { (builder) in
+        //            builder.roomName = "my-room"
+        //
+        //            if let videoTrack = localVideoTrack {
+        //                builder.videoTracks = [ videoTrack ]
+        //            } else {
+        //                print("Couldn't make videoTrack")
+        //            }
+        //        }
+        //
+        //        room = TwilioVideo.connect(with: connectOptions, delegate:self)
+    }
+    
+    func prepareLocalMedia() {
+        
+        // We will share local audio and video when we connect to the Room.
+        // Create an audio track.
+        if (localAudioTrack == nil) {
+            localAudioTrack = TVILocalAudioTrack.init()
+            
+            if (localAudioTrack == nil) {
+                print("Failed to create audio track")
+            }
+        }
+        
+        // Create a video track which captures from the camera.
+        if (localVideoTrack == nil) {
+            self.startPreview()
+        }
     }
     
     @objc func updateGameState(notification: Notification) {
@@ -171,15 +347,121 @@ class TPMainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+// MARK: TVIRoomDelegate
+extension TPMainViewController : TVIRoomDelegate {
+    func didConnect(to room: TVIRoom) {
+        
+        // At the moment, this example only supports rendering one Participant at a time.
+        
+        print("Connected to room \(room.name) as \(String(describing: room.localParticipant?.identity))")
+        
+        if (room.participants.count > 0) {
+            self.participant = room.participants[0]
+            self.participant?.delegate = self
+        }
+    }
+    
+    func room(_ room: TVIRoom, didDisconnectWithError error: Error?) {
+        print("Disconncted from room \(room.name), error = \(String(describing: error))")
+        
+        //        self.cleanupRemoteParticipant()
+        self.room = nil
+        
+        //        self.showRoomUI(inRoom: false)
+    }
+    
+    func room(_ room: TVIRoom, didFailToConnectWithError error: Error) {
+        print("Failed to connect to room with error")
+        self.room = nil
+        print(error.localizedDescription)
+        
+        //        self.showRoomUI(inRoom: false)
+    }
+    
+    func room(_ room: TVIRoom, participantDidConnect participant: TVIParticipant) {
+        if (self.participant == nil) {
+            self.participant = participant
+            self.participant?.delegate = self
+        }
+        print("Room \(room.name), Participant \(participant.identity) connected")
+    }
+    
+    func room(_ room: TVIRoom, participantDidDisconnect participant: TVIParticipant) {
+        if (self.participant == participant) {
+            //            cleanupRemoteParticipant()
+        }
+        print("Room \(room.name), Participant \(participant.identity) disconnected")
+    }
+}
+
+// MARK: TVIParticipantDelegate
+extension TPMainViewController : TVIParticipantDelegate {
+    func participant(_ participant: TVIParticipant, addedVideoTrack videoTrack: TVIVideoTrack) {
+        print("Participant \(participant.identity) added video track")
+        
+        if (self.participant == participant) {
+            setupRemoteVideoView()
+            videoTrack.addRenderer(self.remoteView!)
+        }
+    }
+    
+    func participant(_ participant: TVIParticipant, removedVideoTrack videoTrack: TVIVideoTrack) {
+        print("Participant \(participant.identity) removed video track")
+        
+        if (self.participant == participant) {
+            videoTrack.removeRenderer(self.remoteView!)
+            self.remoteView?.removeFromSuperview()
+            self.remoteView = nil
+        }
+    }
+    
+    func participant(_ participant: TVIParticipant, addedAudioTrack audioTrack: TVIAudioTrack) {
+        print("Participant \(participant.identity) added audio track")
+        
+    }
+    
+    func participant(_ participant: TVIParticipant, removedAudioTrack audioTrack: TVIAudioTrack) {
+        print("Participant \(participant.identity) removed audio track")
+    }
+    
+    func participant(_ participant: TVIParticipant, enabledTrack track: TVITrack) {
+        var type = ""
+        if (track is TVIVideoTrack) {
+            type = "video"
+        } else {
+            type = "audio"
+        }
+        print("Participant \(participant.identity) enabled \(type) track")
+    }
+    
+    func participant(_ participant: TVIParticipant, disabledTrack track: TVITrack) {
+        var type = ""
+        if (track is TVIVideoTrack) {
+            type = "video"
+        } else {
+            type = "audio"
+        }
+        print("Participant \(participant.identity) disabled \(type) track")
+    }
+}
+
+
+
+
+extension TPMainViewController : TVIVideoViewDelegate {
+    func videoView(_ view: TVIVideoView, videoDimensionsDidChange dimensions: CMVideoDimensions) {
+        self.view.setNeedsLayout()
+        print("CHANGED DIMENSIONS")
+    }
+}
+
+// MARK: TVICameraCapturerDelegate
+extension TPMainViewController : TVICameraCapturerDelegate {
+    func cameraCapturer(_ capturer: TVICameraCapturer, didStartWith source: TVICameraCaptureSource) {
+        self.previewView.shouldMirror = (source == .frontCamera)
+    }
+}
+
+
